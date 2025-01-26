596ab02ef812bd0f75c92db9426a190a24273e5b
Alexandre Okita <xande.okita@gmail.com>
2025-01-26 14:57:27 -0300

    fix: fix recipes alpha color

--- Assets/Scripts/BubbleTea.cs
+++ Assets/Scripts/BubbleTea.cs
@@ -13,8 +13,19 @@
             BubbleQtt = bubbleQtt;
         }
 
+        // ReSharper disable Unity.PerformanceAnalysis
         public bool IsCorrect(Recipe recipe)
         {
+            Debug.Log(Color == recipe.Color && AudioClip == recipe.AudioClip && BubbleQtt >= recipe.BubbleQttStart && BubbleQtt <= recipe.BubbleQttEnd);
+            Debug.Log(recipe.Color);
+            Debug.Log(recipe.AudioClip);
+            Debug.Log(recipe.BubbleQttStart);
+            Debug.Log(recipe.BubbleQttEnd);
+            Debug.Log("---------------");
+            
+            Debug.Log(Color);
+            Debug.Log(AudioClip);
+            Debug.Log(BubbleQtt);
             return Color == recipe.Color && AudioClip == recipe.AudioClip && BubbleQtt >= recipe.BubbleQttStart && BubbleQtt <= recipe.BubbleQttEnd;
         }
     }
